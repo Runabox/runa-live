dc2a4d53d379eefdaca0fd6c65c3e81aa6aba086
Runabox <murrellcooper@gmail.com>
2021-11-10 10:45:36 -0500

    First commit

--- README.md
+++
@@ -1,5 +0,0 @@
-# runa.live
-
-Source code of [runa.live](https://runa.live) website
-
-[Original Creator](https://chiya.dev)
--- components/Snowfall.tsx
+++
@@ -1,5 +0,0 @@
-import { chakra } from "@chakra-ui/system";
-import dynamic from "next/dynamic";
-
-const Snowfall = chakra(dynamic(() => import("react-snowfall"), { ssr: false }));
-export default Snowfall;
