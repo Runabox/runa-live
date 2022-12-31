e2126ffc09bf67b80fd4b7dad3cc9ce4cd6dd7f0
Cooper <61383318+Runabox@users.noreply.github.com>
2022-12-31 09:46:56 +0000

    h

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,19 @@
-import '../styles/globals.css'
+import { ChakraProvider, extendTheme } from '@chakra-ui/react';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider theme={extendTheme({
+      intialColorMode: 'dark',
+      styles: {
+        global: (props) => ({
+          body: {
+            bg: '#000000',
+            color: '#ffffff'
+          },
+        }),
+      },
+    })}>
+      <Component {...pageProps} />
+    </ChakraProvider >
+  );
 }
