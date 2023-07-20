1422d61e9bd1ce4cdf82d7a7d40917c17adc4e6d
Runabox <murrellcooper@gmail.com>
2023-07-20 13:57:24 -0400

    update

--- pages/_app.js
+++ pages/_app.js
@@ -1,21 +1,23 @@
-import { ChakraProvider, extendTheme } from '@chakra-ui/react';
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 
 export default function App({ Component, pageProps }) {
   return (
-    <ChakraProvider theme={extendTheme({
-      intialColorMode: 'dark',
-      styles: {
-        global: (props) => ({
-          body: {
-            bg: 'radial-gradient(circle, rgba(71, 10, 145,1) 0%, rgba(109, 14, 198,1) 100%)',
-          },
-          '*': {
-            color: '#ffffff!important'
-          },
-        }),
-      },
-    })}>
+    <ChakraProvider
+      theme={extendTheme({
+        intialColorMode: "dark",
+        styles: {
+          global: (props) => ({
+            body: {
+              bg: "#212121",
+            },
+            "*": {
+              color: "#ffffff!important",
+            },
+          }),
+        },
+      })}
+    >
       <Component {...pageProps} />
-    </ChakraProvider >
+    </ChakraProvider>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,79 +1,252 @@
-import { Avatar, AvatarBadge, Box, Center, Container, Flex, Heading, Image, ScaleFade, Spinner, Text, Tooltip, useToast } from "@chakra-ui/react";
+import {
+  Avatar,
+  AvatarBadge,
+  Box,
+  Center,
+  Container,
+  Flex,
+  Heading,
+  Image,
+  ScaleFade,
+  Spinner,
+  Text,
+  Tooltip,
+  useToast,
+  VStack,
+  Icon,
+  SimpleGrid,
+} from "@chakra-ui/react";
 import Head from "next/head";
 import { useEffect, useState } from "react";
+import { FaDiscord, FaGithub } from "react-icons/fa";
 
 function Index() {
-    const [loading, setLoading] = useState(true);
-    const [status, setStatus] = useState({});
-    const toast = useToast();
+  const [loading, setLoading] = useState(true);
+  const [status, setStatus] = useState({});
+  const [copiedDiscord, setCopiedDiscord] = useState(false);
+  const toast = useToast();
 
-    useEffect(() => {
-        (async () => {
-            let res = await fetch('https://api.discord-status.me/raw/942536039505985557').catch(e => { });
+  useEffect(() => {
+    if (copiedDiscord) setTimeout(() => setCopiedDiscord(false), 3000);
+  }, [copiedDiscord]);
 
-            if (res === undefined || !res.ok)
-                return toast({
-                    status: 'error',
-                    position: 'bottom-right',
-                    title: 'Error',
-                    description: 'Error loading Discord status',
-                });
+  useEffect(() => {
+    (async () => {
+      let res = await fetch(
+        "https://api.discord-status.me/raw/942536039505985557"
+      ).catch((e) => {});
 
-            let result = await res.json().catch(e => { });
-            if (result === undefined)
-                return toast({
-                    status: 'error',
-                    position: 'bottom-right',
-                    title: 'Error',
-                    description: 'Error loading Discord status',
-                });
+      if (res === undefined || !res.ok)
+        return toast({
+          status: "error",
+          position: "bottom-right",
+          title: "Error",
+          description: "Error loading Discord status",
+        });
 
-            console.log(result);
-            setStatus(result);
-            setLoading(false);
-        })();
-    }, []);
+      let result = await res.json().catch((e) => {});
+      if (result === undefined)
+        return toast({
+          status: "error",
+          position: "bottom-right",
+          title: "Error",
+          description: "Error loading Discord status",
+        });
 
-    if (loading)
-        return <Center h='100vh'><Spinner /></Center>;
+      console.log(result);
+      setStatus(result);
+      setLoading(false);
+    })();
+  }, []);
 
+  if (loading)
     return (
-        <>
-            <Head>
-                <title>runa.live</title>
-                <link rel='icon' type="image/png" sizes="2048x2048" href={status.user ? status.user.avatar : ''} />
-            </Head>
+      <Center h="100vh">
+        <Spinner />
+      </Center>
+    );
 
-            <Container maxW='container.xl'>
-                <Center h='100vh'>
-                    <ScaleFade initialScale={0.75} in={true}>
-                        <Box borderRadius={50} w={500} h={250} shadow='0px 0px 40px 30px rgba(71, 10, 145, 0.80)' bg='rgba(71, 10, 145, 0.8)' opacity='80%'>
-                            <Center h='100%'>
-                                <Flex w='90%'>
-                                    <Avatar
-                                        src={status.user.avatar}
-                                        w={150}
-                                        h={150}
-                                    >
-                                        <Tooltip placement='top' label={status['status_text']} bg='rgba(20, 20, 20, 1)' mb={-1} closeDelay={500}>
-                                            <AvatarBadge as={Image} w={50} bg='black' borderWidth={5} borderColor='rgba(0, 0, 0, 0.0)' h={50} src={status['status_image']} />
-                                        </Tooltip>
-                                    </Avatar>
+  return (
+    <>
+      <Head>
+        <title>runa.live</title>
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="2048x2048"
+          href={status.user ? status.user.avatar : ""}
+        />
+      </Head>
 
-                                    <Box ml={5} opacity='55%'>
-                                        <Tooltip placement='top' label={status.user.id} bg='rgba(20, 20, 20, 1)' mb={-4} closeDelay={500}>
-                                            <Text fontSize='250%'>{status.user.username}<span style={{ opacity: '60%' }}>#{status.user.discriminator}</span></Text>
-                                        </Tooltip>
-                                        <Text>full stack developer</Text>
-                                        <Text>react | js | c# | next</Text>
-                                    </Box>
-                                </Flex>
-                            </Center>
-                        </Box>
-                    </ScaleFade>
-                </Center>
-            </Container></>
-    )
+      <Container maxW="container.xl">
+        <Flex
+          flexDir="row"
+          gap="100%"
+          mt={20}
+          p={25}
+          borderRadius={10}
+          w="100%"
+          bg="rgba(0, 0, 0, 0.2)"
+          boxShadow="3px 3px rgba(0, 0, 0, 0.10)"
+          position="relative"
+        >
+          <VStack spacing={4} alignItems="left">
+            <Flex flexDir="row" gap={4} alignItems="center">
+              <Avatar w={100} h={100} src={status.user.avatar} />
+              <VStack alignItems="left" spacing={0}>
+                <Heading fontSize="270%" textShadow="1px 1px black">
+                  {status.user.username}
+                </Heading>
+                <Text opacity="75%" fontSize="150%">
+                  full stack developer
+                </Text>
+              </VStack>
+            </Flex>
+
+            <Text
+              fontSize="130%"
+              opacity="75%"
+              w={400}
+              textShadow="1px 1px black"
+              fontWeight={350}
+            >
+              React / Next | JavaScript | C#
+            </Text>
+          </VStack>
+
+          <VStack
+            alignItems="right"
+            textAlign="right"
+            position="absolute"
+            right={25}
+            spacing={1}
+          >
+            <SocialLink
+              icon={<FaDiscord />}
+              href="runabox"
+              type="copy"
+              text="Discord"
+              copyState={copiedDiscord}
+              setCopyState={setCopiedDiscord}
+            />
+
+            <SocialLink
+              icon={<FaGithub />}
+              href="https://github.com/Runabox"
+              type="link"
+              text="GitHub"
+            />
+          </VStack>
+        </Flex>
+
+        <Center>
+          <Flex
+            h={0}
+            borderColor="rgba(0, 0, 0, 0.2)"
+            borderWidth={1}
+            alignItems="center"
+            justifyContent="center"
+            w="97%"
+            mt={10}
+          >
+            <Box bg="#212121" paddingRight={2} paddingLeft={2}>
+              <Text opacity="50%" fontWeight={500}>
+                PROJECTS
+              </Text>
+            </Box>
+          </Flex>
+        </Center>
+
+        <Center>
+          <Box w="100%" mt={10}>
+            <SimpleGrid columns={{ base: 1, md: 2, xl: 3 }} gap={8} w="100%">
+              <ProjectCard
+                title="upld"
+                description="An easy to use, simple file uploader made for use with ShareX."
+                status="offline"
+                thumbnail="/upld-preview.png"
+                link="https://runa.live"
+              />
+            </SimpleGrid>
+          </Box>
+        </Center>
+      </Container>
+    </>
+  );
+}
+
+function ProjectCard({ title, description, thumbnail, link, status }) {
+  return (
+    <Center>
+      <Box
+        _hover={{ cursor: "pointer", opacity: "80%" }}
+        as={"a"}
+        href={status === "offline" ? undefined : link}
+        target="_blank"
+        borderRadius={10}
+        w={350}
+        boxShadow="3px 3px rgba(0, 0, 0, 0.10)"
+        bg="linear-gradient(356deg, rgba(46,44,166,1) 0%, rgba(0,0,0,1) 53%, rgba(0,0,0,1) 69%)"
+        overflow="hidden"
+      >
+        <Image src={thumbnail} />
+
+        <Box p={3}>
+          <Flex flexDir="row" alignItems="center">
+            <Heading textShadow="2px 2px black">{title}</Heading>
+            <Text textAlign="right" w="100%" opacity="75%">
+              Status: <b>{status === "offline" ? "Offline" : "Online"}</b>
+            </Text>
+          </Flex>
+          <Text opacity="75%" mt={1}>
+            {description}
+          </Text>
+        </Box>
+      </Box>
+    </Center>
+  );
+}
+
+function SocialLink({
+  icon,
+  text,
+  href,
+  type = "link",
+  copyState /* <-- nullable */,
+  setCopyState /* <-- nullable */,
+}) {
+  return (
+    <Tooltip
+      label="Copied"
+      placement="left"
+      bg="rgba(0, 0, 0, 0.8)"
+      mr={1}
+      isDisabled={type !== "copy" ? true : copyState === true ? false : true}
+      isOpen={type === "copy" && copyState === true}
+      hasArrow
+    >
+      <Flex
+        _hover={{ opacity: "60%", cursor: "pointer" }}
+        href={type === "link" ? href : undefined}
+        as={type === "link" ? "a" : undefined}
+        target={type === "link" ? "_blank" : undefined}
+        flexDir="row"
+        gap={1.5}
+        alignItems="center"
+        onClick={
+          type === "copy"
+            ? () => {
+                setCopyState(true);
+                navigator.clipboard.writeText(href);
+              }
+            : undefined
+        }
+        opacity="80%"
+      >
+        {icon} <Text fontSize="110%">{text}</Text>
+      </Flex>
+    </Tooltip>
+  );
 }
 
 export default Index;
